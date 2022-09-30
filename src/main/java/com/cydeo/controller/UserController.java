7fcd6a93ee73e14ac750f949e679d9c52b0f05c6
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-30 16:52:44 -0500

    Practicing Thymeleaf

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -33,8 +33,8 @@ public class UserController {
     public String insertUser(Model model, @ModelAttribute("user") UserDTO user){
 
         userService.save(user);
-        model.addAttribute("users", userService.findAll());
-        return "/user/create";
+
+        return "redirect:/user/create";
     }
 
 }
